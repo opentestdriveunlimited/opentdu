5775a97d88122aeb9cddecfbbf7da29ac5fa8004
opentdu <opentdu@tutamail.com>
2025-05-25 19:23:29 -0700

    Flash - Groundwork for flash player (not reversed yet)

---
+++ source/flash/flash_player.cpp
@@ -0,0 +1,25 @@
+#include "shared.h"
+#include "flash_player.h"
+
+FlashPlayer::FlashPlayer()
+{
+}
+
+FlashPlayer::~FlashPlayer()
+{
+}
+
+void FlashPlayer::showFrame(const char *param_1, const char *param_2, bool param_3)
+{
+    OTDU_UNIMPLEMENTED; // TODO:
+}
+
+void FlashPlayer::setVariableValue(const char *param_1, const char *param_2)
+{
+    OTDU_UNIMPLEMENTED; // TODO:
+}
+
+void FlashPlayer::setVariableValue(const char *param_1, const uint32_t param_2)
+{
+    OTDU_UNIMPLEMENTED; // TODO:
+}
---
+++ source/flash/flash_player.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class FlashPlayer {
+public:
+    FlashPlayer();
+    ~FlashPlayer();
+
+    void showFrame(const char* param_1, const char* param_2, bool param_3);
+    void setVariableValue(const char* param_1, const char* param_2);
+    void setVariableValue(const char *param_1, const uint32_t param_2);
+};
