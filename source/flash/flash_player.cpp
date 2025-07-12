5d711a160491b8af94ab8462098ccea41a6d5f16
opentdu <opentdu@tutamail.com>
2025-07-11 17:12:01 -0700

    Flash - Added overload to bind int64 vars to FlashPlayer

--- source/flash/flash_player.cpp
+++ source/flash/flash_player.cpp
@@ -24,6 +24,11 @@ void FlashPlayer::setVariableValue(const char *param_1, const uint32_t param_2)
     OTDU_UNIMPLEMENTED; // TODO:
 }
 
+void FlashPlayer::setVariableValue(const uint64_t param_1, const uint64_t param_2)
+{
+    OTDU_UNIMPLEMENTED; // TODO:
+}
+
 int32_t FlashPlayer::getVariableAsInt(const char* param_1)
 {
     OTDU_UNIMPLEMENTED; // TODO:
--- source/flash/flash_player.h
+++ source/flash/flash_player.h
@@ -8,6 +8,7 @@ public:
     void showFrame(const char* param_1, const char* param_2, bool param_3);
     void setVariableValue(const char* param_1, const char* param_2);
     void setVariableValue(const char *param_1, const uint32_t param_2);
+    void setVariableValue(const uint64_t param_1, const uint64_t param_2);
 
     int32_t getVariableAsInt(const char* param_1);
 };
