526c53d5d8eb009ea661c3dd2d9c504afd3218de
opentdu <opentdu@tutamail.com>
2025-07-13 17:55:17 -0700

    File - Implemented VFT for StreamedResource

--- source/filesystem/streamed_resource.h
+++ source/filesystem/streamed_resource.h
@@ -1,13 +1,31 @@
 #pragma once
 
+class TestDriveThread;
+
 class StreamedResource {
 public:
     StreamedResource()
-    : bRegistered( false )
+    : bWaitingForInstanciation( true )
     {
         
     }
 
+    virtual void instanciate()
+    {
+        OTDU_LOG_WARN("StreamedResource::instanciate() is unimplemented!!\n");
+        OTDU_ASSERT(false);
+    }
+
+    virtual void instanciate(TestDriveThread* param_1)
+    {
+
+    }
+
+    inline void markAsInstanciated()
+    {
+        bWaitingForInstanciation = false;
+    }
+
 protected:
-    uint8_t bRegistered : 1;
+    uint8_t bWaitingForInstanciation : 1;
 };
