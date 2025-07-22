2482ee50e1f8e5d21bd322515e8254a994f74229
opentdu <opentdu@tutamail.com>
2025-07-21 22:46:54 -0700

    Core - Skip debug log if verbose build flag is not set

--- source/core/logger.h
+++ source/core/logger.h
@@ -16,7 +16,11 @@ public:
 #define OTDU_LOG_INFO( format, ... ) GlobalLogger::Write( "[LOG]\t" __FILE__  ":" OTDU_STRINGIFY( __LINE__ ) " >> " format, ##__VA_ARGS__ )
 #define OTDU_LOG_WARN( format, ... ) GlobalLogger::Write( "[WARN]\t" __FILE__  ":" OTDU_STRINGIFY( __LINE__ ) " >> " format, ##__VA_ARGS__ )
 #define OTDU_LOG_ERROR( format, ... ) GlobalLogger::Write( "[ERROR]\t" __FILE__  ":" OTDU_STRINGIFY( __LINE__ ) " >> " format, ##__VA_ARGS__ )
+#if OTDU_VERBOSE_LOG
 #define OTDU_LOG_DEBUG( format, ... ) GlobalLogger::Write( "[DEBUG]\t" __FILE__ ":" OTDU_STRINGIFY( __LINE__ ) " >> " format, ##__VA_ARGS__ )
+#else 
+#define OTDU_LOG_DEBUG( format, ... )
+#endif
 #else
 #define OTDU_LOG_INFO( format, ... )
 #define OTDU_LOG_WARN( format, ... )
