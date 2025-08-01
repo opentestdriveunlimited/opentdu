c936a25519cac45a18a7ab5a99d67b1fd401d494
opentdu <opentdu@tutamail.com>
2025-07-31 22:29:24 -0700

    Movie - Implmented init for MngMovie

--- source/movie/mng_movie.cpp
+++ source/movie/mng_movie.cpp
@@ -1,19 +1,39 @@
 #include "shared.h"
 #include "mng_movie.h"
 
+MngMovie* gpMngMovie = nullptr;
+
 MngMovie::MngMovie()
     : Manager()
+    , render2DM{ Render2DM(), Render2DM() }
+    , drawList()
 {
+    for (uint32_t i = 0; i < kNumEnqueuedFrames; i++ ) {
+        render2DBUnk[i] = Render2DB();
+        hashcodeUnk[i] = 0ull;
+    }
 
+    gpMngMovie = this;
 }
 
 MngMovie::~MngMovie()
 {
-
+    gpMngMovie = nullptr;
 }
 
 bool MngMovie::initialize( TestDriveGameInstance* pGameInstance )
 {
+    DrawStreams local_8;
+    local_8.Normals = '\0';
+    local_8.Specular = '\0';
+    local_8.Tangent = '\0';
+    local_8.Binormal = '\0';
+    local_8.UVMap = '\x01';
+    local_8.Index = '\x01';
+    local_8.Diffuse = '\x01';
+
+    drawList.initialize( 1, 2, 8, false, &local_8, "Movie", true );
+
     return false;
 }
 
--- source/movie/mng_movie.h
+++ source/movie/mng_movie.h
@@ -2,6 +2,10 @@
 
 #include "game/mng_base.h"
 
+#include "render/2db.h"
+#include "render/2dm.h"
+#include "render/draw_list.h"
+
 class MngMovie : public Manager {
 public:
     const char* getName() const override { return "Movie"; }
@@ -13,4 +17,24 @@ public:
 	bool initialize( TestDriveGameInstance* ) override;
 	void tick( float, float ) override;
 	void terminate() override;
+
+private:
+	static constexpr uint32_t kNumEnqueuedFrames = 8;
+
+private:
+	Render2DM render2DM[2];
+	DrawList drawList;
+	
+	// TODO: Might be frames for Bink?
+	Render2DB render2DBUnk[kNumEnqueuedFrames];
+	uint64_t hashcodeUnk[kNumEnqueuedFrames];
+
+	uint8_t bEnabled : 1;
+	uint8_t bInitialized : 1;
+	uint8_t bPlaying : 1;
+	uint8_t bDone : 1;
+
+	int32_t frameIndex;
 };
+
+extern MngMovie* gpMngMovie;
