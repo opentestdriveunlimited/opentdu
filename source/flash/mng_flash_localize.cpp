7b4c8dbd1d6bba2aa0a73e7f6c58c32facb4f3cd
opentdu <opentdu@tutamail.com>
2025-08-27 09:01:34 -0700

    Flash - Added incomplete localized string fetch funcs

--- source/flash/mng_flash_localize.cpp
+++ source/flash/mng_flash_localize.cpp
@@ -4,12 +4,14 @@
 #include "flash_player.h"
 #include "tdu_instance.h"
 
+#include "database/gs_database.h"
+
 MngFlashLocalize* gpMngFlashLocalize = nullptr;
 char MngFlashLocalize::VersionString[64] = "VERSION UKNOW";
 
 MngFlashLocalize::MngFlashLocalize()
     : Manager()
-    , unknown( 0x0 )
+    , dbContext( 0x0 )
 {
     localizedStrings.reserve( 32 );
     reset();
@@ -38,15 +40,69 @@ void MngFlashLocalize::reset()
     resetMenuLocalization();
 }
 
-int32_t MngFlashLocalize::FUN_00716f60(uint64_t param_1)
+int32_t MngFlashLocalize::FUN_00716f60(char* param_1)
 {
     OTDU_UNIMPLEMENTED;
     return -1;
 }
 
-void MngFlashLocalize::FUN_00717ff0(FlashPlayer* param_2, uint64_t param_3, uint64_t param_4, uint32_t param_5)
+void MngFlashLocalize::FUN_00717ff0(FlashPlayer* param_2, char* param_3, char* param_4, void* param_5)
 {
+    // FUN_00717ff0
+    if (param_3 != nullptr) {
+        size_t pcVar3 = strlen(param_3);
+        if (pcVar3 < 0x41) {
+            int32_t iVar4 = FUN_00716f60(param_3);
+            if (iVar4 < 0) {
+                param_2->setVariableValue(param_4, "(ERR:1)");
+                return;
+            }
+            
+            int32_t iVar5 = 0;
+            char* local_228[10] = {
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr,
+                nullptr
+            };
+
+            char** ppcVar7 = nullptr;
+            if (0 < iVar4) {
+                int32_t iVar6 = 0;
+                ppcVar7 = &param_3;
+                do {
+                    ppcVar7 = ppcVar7 + 1;
+                    if ((iVar5 < 0) || (iVar6 = iVar5, 9 < iVar5)) {
+                        iVar6 = 0;
+                    }
+                    local_228[iVar6] = *ppcVar7;
+                    if ((iVar5 < 0) || (iVar6 = iVar5, 9 < iVar5)) {
+                        iVar6 = 0;
+                    }
+                } while ((local_228[iVar6] != (char *)0x0) && (iVar5 = iVar5 + 1, iVar5 < iVar4));
+            }
+            ppcVar7 = local_228;
+          
+            OTDU_UNIMPLEMENTED;
+        }
+        OTDU_UNIMPLEMENTED;
+    }
+
     OTDU_UNIMPLEMENTED;
+}
+
+void MngFlashLocalize::FUN_00718120(FlashPlayer *param_2, char *param_3, uint64_t param_4)
+{
+    // FUN_00718120
+    char* pcVar1 = gpDatabase->getStringByHashcode(dbContext, param_4);                       
+    void* pcVar3 = nullptr; //FUN_00416450(local_2c,(char *)&param_1); // TODO: Not sure wtf this is (the arg is never consumed by FUN_00717ff0)
+    FUN_00717ff0(param_2, param_3, pcVar1, pcVar3);
 }
 
 void  MngFlashLocalize::resetMenuLocalization()
--- source/flash/mng_flash_localize.h
+++ source/flash/mng_flash_localize.h
@@ -18,13 +18,14 @@ public:
     virtual bool initialize( TestDriveGameInstance* ) override;
     void reset() override;
 
-    int32_t FUN_00716f60(uint64_t param_1);
-    void FUN_00717ff0(FlashPlayer* param_2, uint64_t param_3, uint64_t param_4, uint32_t param_5);
+    int32_t FUN_00716f60(char* param_1);
+    void FUN_00717ff0(FlashPlayer* param_2, char* param_3, char* param_4, void* param_5 = nullptr);
+    void FUN_00718120(FlashPlayer* param_2, char* param_3, uint64_t param_4);
 
 private:
     std::vector<std::tuple<std::string, std::string>> localizedStrings;
     std::vector<std::tuple<std::string, std::string>> menuLocalizedStrings;
-    int32_t unknown;
+    int32_t dbContext;
 
 private:
     void resetMenuLocalization();
