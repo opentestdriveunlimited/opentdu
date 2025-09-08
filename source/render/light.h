eda1589c8315ac46f23af0a5a9192563805e2b63
opentdu <opentdu@tutamail.com>
2025-09-08 14:01:29 -0700

    Render - Added missing point light declaration

--- source/render/light.h
+++ source/render/light.h
@@ -2,7 +2,7 @@
 
 enum class eLightType {
     LT_Invalid = 0,	
-    LT_Ambient	= 1,	
+    LT_Ambient = 1,	
     LT_Directional = 2,	
     LT_Point = 3,	
     LT_Spot = 4,	
@@ -22,6 +22,9 @@ struct Light {
 struct DirectionalLight : public Light {
      DirectionalLight()
         : Light(eLightType::LT_Directional)
+        , Diffuse(Eigen::Vector4f::Zero())
+        , Ambient(Eigen::Vector4f::Zero())
+        , Specular(Eigen::Vector4f::Zero())
         , Direction( Eigen::Vector3f::Zero() )
     {
         
@@ -36,9 +39,33 @@ struct DirectionalLight : public Light {
 struct AmbientLight : public Light {
      AmbientLight()
         : Light(eLightType::LT_Ambient)
+        , Color(Eigen::Vector4f::Zero())
     {
         
     }
 
     Eigen::Vector4f Color;
+};
+
+struct PointLight : public Light {
+     PointLight()
+        : Light(eLightType::LT_Point)
+        , Diffuse(Eigen::Vector4f::Zero())
+        , Ambient(Eigen::Vector4f::Zero())
+        , Specular(Eigen::Vector4f::Zero())
+        , Position(Eigen::Vector4f::Zero())
+        , Attenuated(0)
+        , AttenuationCutoff(0.0f)
+        , AttenuationDropoff(0.0f)
+    {
+        
+    }
+
+    Eigen::Vector4f Diffuse;
+    Eigen::Vector4f Ambient;
+    Eigen::Vector4f Specular;
+    Eigen::Vector4f Position;
+    int32_t Attenuated;
+    float AttenuationCutoff;
+    float AttenuationDropoff;
 };
